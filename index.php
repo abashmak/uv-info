1e1e6f635a99e99563eec0d9700ba4b2b12a410d
Dmitry Efimenko <dmitrief@gmail.com>
2026-03-15 11:00:11 -0700

    fix: scroll issues

--- index.php
+++ index.php
@@ -1,47 +1,46 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-<title>UV Info</title>
+  <title>UV Info</title>
 
-<link rel="stylesheet" href="css/styles.css?v=<?php echo filemtime('css/styles.css'); ?>">
+  <link rel="stylesheet" href="css/styles.css?v=<?php echo filemtime('css/styles.css'); ?>">
 
-<link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
-
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap" rel="stylesheet">
 </head>
 
 <body>
+  <header>UV Info</header>
 
-<header>UV Info</header>
-
-<div class="container">
+  <div class="container">
     <div id="uvbox">
-        <div id="spinner" class="spinner"></div>
+      <div id="spinner" class="spinner"></div>
 
-        <div id="currentTime" class="current-time"></div>
+      <div id="currentTime" class="current-time"></div>
 
-        <div id="uvvalue">--</div>
+      <div id="uvvalue">--</div>
 
-        <div id="risk"></div>
+      <div id="risk"></div>
 
-        <div id="forecast" class="forecast"></div>
+      <div id="forecast" class="forecast"></div>
 
-        <div id="sun-times" class="sun-times"></div>
+      <div id="sun-times" class="sun-times"></div>
 
-        <div id="exposure-times" class="exposure-times">
-            <div id="exposure-times-title" class="exposure-times-title">
-                Safe exposure times:
-            </div>
-
-            <ul></ul>
+      <div id="exposure-times" class="exposure-times">
+        <div id="exposure-times-title" class="exposure-times-title">
+          Safe exposure times:
         </div>
+
+        <ul></ul>
+      </div>
     </div>
-</div>
+  </div>
 
-<script src="js/app.js?v=<?php echo filemtime('js/app.js'); ?>"></script>
-
+  <script src="js/app.js?v=<?php echo filemtime('js/app.js'); ?>"></script>
 </body>
+
 </html>
